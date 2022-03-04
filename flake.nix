e7bb7fd2cbc0e7a25bc5a3619e344be9c75bd258
John Soo <john.soo@arista.com>
2022-03-03 23:33:04 -0800

    Keep nixpkgs @ 21.11 where nix is v2.3.16.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Hydra's builtin hydra-eval-jobs as a standalone";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
