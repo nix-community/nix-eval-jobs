866fae0336ed9bc74f293b86b19a957fb13de953
John Soo <john.soo@arista.com>
2022-03-07 10:50:22 -0800

    Revert "Use nix@2.3.16."
    This reverts commit 88ae2262b566900e1d945630e9aadd910e0382b8.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   description = "Hydra's builtin hydra-eval-jobs as a standalone";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        drvArgs = { srcDir = self; nix = pkgs.nix; };
+        drvArgs = { srcDir = self; nix = pkgs.nixUnstable; };
       in
       rec {
         packages.nix-eval-jobs = pkgs.callPackage ./default.nix drvArgs;
