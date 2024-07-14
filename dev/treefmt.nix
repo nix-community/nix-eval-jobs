2258dc78104bb6ea0164e2532cbd33837a9258da
Jörg Thalheim <joerg@thalheim.io>
2024-07-14 05:48:47 +0200

    update treefmt

--- dev/treefmt.nix
+++ dev/treefmt.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   # Used to find the project root
   projectRootFile = "flake.lock";
 
@@ -6,39 +7,9 @@
 
   programs.clang-format.enable = true;
 
-  settings.formatter = {
-    nix = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.deadnix}/bin/deadnix --edit "$@"
-
-          for i in "$@"; do
-            ${pkgs.statix}/bin/statix fix "$i"
-          done
-
-          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt "$@"
-        ''
-        "--"
-      ];
-      includes = [ "*.nix" ];
-      excludes = [ ];
-    };
-
-    clang-format = { };
-
-    python = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.python3.pkgs.black}/bin/black "$@"
-          ${pkgs.ruff}/bin/ruff --fix "$@"
-        ''
-        "--" # this argument is ignored by bash
-      ];
-      includes = [ "*.py" ];
-    };
-  };
+  programs.deadnix.enable = true;
+  programs.nixfmt.enable = true;
+  programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+  programs.ruff.format = true;
+  programs.ruff.check = true;
 }
