8e82a79fe9329f3be4836ad45d4edf2383e7723a
Jörg Thalheim <joerg@thalheim.io>
2024-12-10 08:28:57 +0100

    bump 2.25.0

--- default.nix
+++ default.nix
@@ -13,7 +13,7 @@ let
 in
 stdenv.mkDerivation {
   pname = "nix-eval-jobs";
-  version = "2.24.1";
+  version = "2.25.0";
   src = if srcDir == null then filterMesonBuild ./. else srcDir;
   buildInputs = with pkgs; [
     nlohmann_json
