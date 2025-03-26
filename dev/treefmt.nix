ef1b42baefc253fd3b937b5a831c8b8a4a88e095
Jörg Thalheim <joerg@thalheim.io>
2025-03-26 16:06:31 +0100

    treefmt: disable formatter if not support on a platform

--- dev/treefmt.nix
+++ dev/treefmt.nix
@@ -1,16 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  supportsNix =
+    lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt.compiler
+    && (builtins.tryEval pkgs.nixfmt.compiler.outPath).success;
+  supportsDeno =
+    lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.deno
+    && (builtins.tryEval pkgs.deno.outPath).success;
+in
 {
   # Used to find the project root
   projectRootFile = "flake.lock";
 
-  programs.deno.enable = true;
+  programs.deno.enable = supportsDeno;
   programs.yamlfmt.enable = true;
 
   programs.clang-format.enable = true;
   programs.clang-format.package = pkgs.llvmPackages_latest.clang-tools;
 
   programs.deadnix.enable = true;
-  programs.nixfmt.enable = true;
+  programs.nixfmt.enable = supportsNix;
   programs.ruff.format = true;
   programs.ruff.check = true;
 }
