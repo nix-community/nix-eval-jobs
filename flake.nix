d963aacfdd147147a29073ad92b61982c8b410a5
Jean-François Roche <jfroche@pyxel.be>
2025-10-07 12:06:31 +0200

    Update nixpkgs input url to use tarball

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Hydra's builtin hydra-eval-jobs as a standalone";
 
-  # Switch back after https://nixpk.gs/pr-tracker.html?pr=396710 is finished
-  # inputs.nixpkgs.url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
   inputs.nix = {
     url = "github:NixOS/nix/2.31-maintenance";
     # We want to control the deps precisely
